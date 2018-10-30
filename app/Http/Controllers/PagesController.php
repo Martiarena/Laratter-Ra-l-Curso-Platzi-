56df4bb346c509a45cbf1906e2b93cfff729d407
Raúl Martiarena <raulmartiarena89@gmail.com>
2018-10-30 07:43:06 -0500

    Avance
    Curso video 8

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,22 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Message;
 use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    public function home(){
-    	$links = [
-	    	'https://platzi.com/laravel' => 'Curso de Laravel',
-	    	'https://laravel.com' => 'Página de Laravel'
-	    ];
-	    return view('welcome', [
-	    	//'teacher' => 'Guido Contreras Woda',
-	    	'links' => $links,
-	    ]);
-    }
+    public function home()
+    {
+    	$messages = [
+            [
+                'id' => 1,
+                'content' => 'Este es mi primer mensaje!',
+                'image' => 'http://lorempixel.com/600/338?1',
+            ],
+            [
+                'id' => 2,
+                'content' => 'Este es mi segundo mensaje!',
+                'image' => 'http://lorempixel.com/600/338?2',
+            ],
+            [
+                'id' => 3,
+                'content' => 'Otro mensaje más!',
+                'image' => 'http://lorempixel.com/600/338?3',
+            ],
+            [
+                'id' => 4,
+                'content' => 'El ultimo mensaje!',
+                'image' => 'http://lorempixel.com/600/338?4',
+            ],
+        ];
 
-    public function aboutUs(){
-    	return view('about');
+	    return view('welcome', [
+	    	'messages' => $messages,
+		]);
     }
 }
